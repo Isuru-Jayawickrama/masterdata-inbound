f24cb04deb8413841bde916bd7c08579b3d2b683
Isuru Jayawickrama <Isuru.Jayawickrama@sysco.com>
2026-08-07 14:36:06 +0530

    Initial project structure created

---
+++ src/main/java/com/sysco/masterdata_inbound/MasterdataInboundApplication.java
@@ -0,0 +1,13 @@
+package com.sysco.masterdata_inbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterdataInboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterdataInboundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysco/masterdata_inbound/MasterdataInboundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysco.masterdata_inbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterdataInboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
